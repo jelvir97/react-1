2d8a59269abe089adbc485da948f3c8578413ecf
Juan Elvir <jelvir97@gmail.com>
2024-01-01 17:07:32 -0700

    addCommas and tests

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,22 @@
-function addCommas() {}
+function addCommas(num) {
+    const numStr = num + ''
+    const decimalIdx = numStr.indexOf('.')
+    let decimals = ''
+    let arr = numStr.split('')
+
+    if(decimalIdx>-1){
+        decimals = arr.slice(decimalIdx).join('')
+        arr = arr.slice(0,decimalIdx)
+    }
+
+    let i = arr.length -3
+    while(i>0){
+        arr.splice(i, 0, ',')
+        i -=3
+    }
+    return arr.join('') + decimals
+}
+
+//arr. splice(idx, num to replace, value to insert)
 
 module.exports = addCommas;
